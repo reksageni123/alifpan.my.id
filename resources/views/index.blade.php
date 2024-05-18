2a27b96f63026617dcbb9f688dc8bd9933cac740
reksageni123 <alifpandoyo40@gmail.com>
2024-05-18 15:40:50 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alif Portfolio /></title>
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+
+  
+</head>
+    <body class="bg-slate-600">
+        <nav class="flex flex-wrap justify-between w-full py-2 bg-gray-500">
+            <div class="ml-[800px]">
+            <div class="space-x-8">
+                <a href="#" class="text-white font-extrabold text-base">Home</a>
+                <a href="#About" class="text-white font-extrabold text-base">About</a>
+                <a href="#Portfolio" class="text-white font-extrabold text-base">Portofolio</a>
+                <a href="#Contact" class="text-white font-extrabold text-base">Contact</a>
+            </div>
+        </div>
+        </nav>
+
+        <section name=" home" id="home" class="max-w-full mx-auto flex py-48 px-0 justify-between">
+            <div class="ml-20 text-2xl">
+            <h1 class="text-2xl text-white ml-40">Hello I'am</span></h1>
+            <h1 class="text-7x1 text-white ml-40">Moh Alif Pandoyo</h1>
+            <p class="text-2xl text-white mt-40 ml-40">Skill</p>
+            <p class="text-7x1 text-white ml-40">> Internet Research 50%</p>
+            <p class="text-7x1 text-white ml-40">> Programer 30%</p>
+            <p class="text-7x1 text-white ml-40">> Desain Grafis 50%</p>
+        </div>
+
+        <div>
+            <img src="https://i.ibb.co/t8xLFgt/f20aeeff-bc9d-4fd0-96f9-0dad6d681a97-1.png" alt="">
+        </div>
+       </section>
+
+       <section name="about" id="About" class="max-w-full mx-auto flex py-32 px-0 justify-center">
+        <div class="ml-[50px]">
+            <div class="mx-auto block">
+                <h1 class="text-2xl text-white text-py-10 ml-80 font blod">About Me</h1>
+                <p class="text-2xl text-white text-py-10 font blod">Saya adalah Mahasiswa D3 Teknik Komputer Kampus Politeknik Harapan</p>
+                <p class="text-2xl text-white text-sl ml 2 text-center">Bersama Tegal dan sekarang sedang Semester 4</p>
+            </div>
+        </div>
+       </section>
+
+       <section name="Portofolio" id="Portfolio" class="max-w-full mx-auto flex py-10 px-0 justify-center">
+        <div class="ml-[50px] ">
+        
+        <div class="mx-auto block">
+            <h1 class=" text-white text-2xl text-center font-bold">Portofolio</h1>
+
+        <div class="justify-center items-center">
+            <img src="https://i.ibb.co/D529wd9/image-1.png" alt="" class="h-[300px] py-5 ml-44">
+        </div>
+
+        <div>
+            <img src="https://i.ibb.co/WtmTQH1/image-2.png" alt="" class="h-[300px] py-5 ml-44">
+        </div>
+
+    </div>
+
+    <section name="Contact" id="Contact" class="max-w-full mx-auto flex py-20 px-0 justify-between">
+        <div class="ml-30">
+            <div class="mx-auto block">
+                <h1 class="text-white text-2xl py-10 text-center font-bold">Contact</h1>
+                <div class="text-center flex flex-wrap items-center space-x-4">
+                    <a href="" class="flex items-center">
+                        <img src="https://i.ibb.co/8cVdMzL/logos-whatsapp-icon.png" alt="" class="w-10 h-10 mr-2">
+                        <span class="text-white py-8">082136962668</span>
+                    </a>
+                    <a href="" class="flex items-center">
+                        <img src="https://i.ibb.co/rtxSvbk/logos-google-gmail.png" alt="" class="w-10 h-10 mr-2">
+                        <span class="text-white py-8">alifpandoyo40@gmail.com</span>
+                    </a>
+                    <a href="" class="flex items-center">
+                        <img src="https://i.ibb.co/fF119Mt/logos-google-maps.png" alt="" class="w-6 h-6 mr-2">
+                        <span class="text-white py-8">Jl Mangga Rt 04 Rw 04 Kel Procot Kab Tegal</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+
+
+</body>
+</html>
